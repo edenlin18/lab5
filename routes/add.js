f289e3335c7d122e39b92a55fc219c6e4e20de1a
Eden Lin <edenlin18@gmail.com>
2016-02-02 19:52:50 +0000

    strstretch goal done

--- routes/add.js
+++ routes/add.js
@@ -5,10 +5,12 @@ exports.addFriend = function(req, res) { 
 	// Your code goes here
 	var name = req.query.name;
 	var description = req.query.description;
+	var hobby = req.query.hobby;
 	var newFriend = {
 		"name": name,
 		"description": description,
-		"imageURL": "http://lorempixel.com/400/400/people"
+		"imageURL": "http://lorempixel.com/400/400/people",
+		"hobby": hobby
 	}
 	console.log(newFriend);
 	data["friends"].push(newFriend);
